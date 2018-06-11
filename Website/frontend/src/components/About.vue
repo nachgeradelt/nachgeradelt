c3854983b833d08a7d3a515f3b11c1fe0cf55654
Unknown <gerdesk@posteo.org>
2018-06-11 22:06:35 +0200

    Prepare 'about us' page

--- Website/frontend/src/components/About.vue
+++ Website/frontend/src/components/About.vue
@@ -3,9 +3,24 @@
   <div class="container about">
     <div class="row">
       <div class="col-md-12">
-        <p>velo Open Access: Wir machen das regionale #Radfahrerwissen wieder sichtbar.
-        Die Behauptung: Alten Tourenbücher funktionieren noch, denn Orte, Wege und Straßen sind meist noch da, wo sie um 1900 waren.
-        Idee: Eine Webanwendung, um diese Behauptung exemplarisch für eine historische Radtourenbeschreibungen um Leipzig zu überprüfen und, um ggf. anschließend als Event zu veranstalten und Nachahmer zu aktivieren. Über Filterfunktionen soll digitalisiertes Wissen auch von (anderen) Kulturgütern entlang der Strecke darstellbar sein. Die Webanwendung soll im Idealfall Nachnutzung ermöglichen. In der Pilotphase ensteht eine Tour bei oder um Leipzig.</p>
+        <p>Das Projekt Nachgeradelt.de entstand im Frühjahr 2018 im Rahmen des
+          <a href="https://codingdavinci.de/events/ost/">Coding da Vinci Ost</a>. Wir sind RadfahrerInnen, HackerInnen, DesignerInnen und geschichtsinteressierte Menschen,
+          die gemeinsam von den Fragen angetrieben werden: Wie ging es RadfahrerInnen um die vorletzte Jahrtausendwende?
+          Unter welchen Bedingungen und auf welchen Straßen radelten sie? Wie unterschied sich das Radfahren von den heutigen
+          Verhältnissen?
+        </p>
+        <p>Das Projektteam sammelte alle digitalisierten Touren im Umfeld von Leipzig aus dem Datensatz der
+          <a href="https://codingdavinci.de/daten/#slub-dresden">Sächsischen Landesbibliothek - Staats- und Universitätsbibliothek Dresden (SLUB).</a> Anschließend wurde eine Strecke
+          ausgewählt, testweise befahren und Stationen entlang des Weges digital mit Zusatzinformationen angereichert. Die
+          <a href="https://github.com/nachgeradelt/nachgeradelt/blob/master/tourenverzeichnis.csv">komplette Liste aller Touren</a> sowie der Quellcode können auf
+          <a href="https://github.com/nachgeradelt/nachgeradelt">Github</a> eingesehen werden.
+        </p>
+        <figure>
+          <img src="../assets/about.jpeg" alt="Über uns">
+          <figcaption>
+            Jens, Johannes, Birte (nicht im Bild: Tobias, Katharina, Justus, Gerd, Jens & Alejandro)
+          </figcaption>
+        </figure>
       </div>
     </div>
   </div>
@@ -17,10 +32,18 @@ export default {
 }
 </script>
 <!-- styling for the component -->
-<style>
-.about {
-  text-align: center;
-  font-size: 20px;
-  margin-top: 60px;
-}
+<style scoped>
+  .about {
+    text-align: center;
+    font-size: 18px;
+    margin-top: 10px;
+  }
+
+  .about p {
+   margin-bottom: 10px;
+  }
+  .about img {
+    max-width: 100%;
+    height: 400px;
+  }
 </style>
