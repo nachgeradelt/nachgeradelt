cddc80926e6fcc1f51e36ebaf428e0ca0f8cf53c
Unknown <gerdesk@posteo.org>
2018-06-17 20:44:54 +0200

    Add tour video

--- Website/frontend/src/components/About.vue
+++ Website/frontend/src/components/About.vue
@@ -30,6 +30,7 @@
             </figcaption>
           </figure>
         </div>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/PXxpIzGcK9M?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
     </div>
   </div>
