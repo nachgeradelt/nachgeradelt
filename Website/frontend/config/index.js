790e57e8057acdcf67c0d1871f17593ed266fc72
ges11dcs <ges11dcs@studserv.uni-leipzig.de>
2019-05-02 15:15:01 +0200

    Remove Cros Error

--- Website/frontend/config/index.js
+++ Website/frontend/config/index.js
@@ -11,6 +11,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
+    //headers: { "Access-Control-Allow-Origin": "http://localhost:8080", "Access-Control-Allow-Credentials": "true" },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
